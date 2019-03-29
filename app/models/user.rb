0b5db47400ffcda6784a43f06e7bff2fb576cf72
thejosk <tkondubhatla@connamara.com>
2019-03-29 16:04:51 -0500

    add devise

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #Validations
+  
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :date_of_birth, presence: true
+
+end
